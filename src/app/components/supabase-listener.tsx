7bff7ff71608941078e1e454b2a9272fe4856e11
Shotaro Hirose <shotaro199959@icloud.com>
2023-12-31 18:33:14 +0900

    Add: login and signup feature

---
+++ src/app/components/supabase-listener.tsx
@@ -0,0 +1,18 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import Navigation from './navigation'
+import type { Database } from '@/lib/database.types'
+
+const SupabaseListener = async () => {
+  const supabase = createServerComponentClient<Database>({ cookies })
+
+  // セッションの取得
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+
+  return <Navigation session={session} />
+}
+
+export default SupabaseListener
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+import type { Database } from '@/lib/database.types'
+
 // メインページ
-const Home = () => {
+const Home = async () => {
+  const supabase = createServerComponentClient<Database>({cookies})
+
+  const { data: { session } } = await supabase.auth.getSession()
+
   return (
     <div>
-      <div>Main</div>
+      {session ? <div>ログイン済</div> : <div>未ログイン</div>}
     </div>
   )
 }
