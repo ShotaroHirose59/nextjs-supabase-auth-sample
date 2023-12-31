c498b9e684ed1cac0f5273e81f1ced5345a7f5be
Shotaro Hirose <shotaro199959@icloud.com>
2023-12-31 18:51:43 +0900

    Add プロフィール編集機能

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -1,16 +1,35 @@
 'use client'
 
 import Link from 'next/link'
+import useStore from '@/store'
+import Image from 'next/image'
+import { useEffect } from 'react'
 import type { Session } from '@supabase/auth-helpers-nextjs'
+import type { Database } from '@/lib/database.types'
+type ProfileType = Database['public']['Tables']['profiles']['Row']
 
 // ナビゲーション
 const Navigation = ({
   session,
+  profile,
 }: {
   session: Session | null
+  profile: ProfileType | null
 }) => {
-  
 
+  const { setUser } = useStore()
+
+  // 状態管理にユーザー情報を保存
+  useEffect(() => {
+    setUser({
+      id: session ? session.user.id : '',
+      email: session ? session.user.email! : '',
+      name: session && profile ? profile.name : '',
+      introduce: session && profile ? profile.introduce : '',
+      avatar_url: session && profile ? profile.avatar_url : '',
+    })
+  }, [session, setUser, profile])
+  
   return (
     <header className="shadow-lg shadow-gray-100">
       <div className="py-5 container max-w-screen-sm mx-auto flex items-center justify-between">
@@ -22,15 +41,14 @@ const Navigation = ({
           {session ? (
             <div className="flex items-center space-x-5">
               <Link href="/settings/profile">
-                {/* <div className="relative w-10 h-10">
+                <div className="relative w-10 h-10">
                   <Image
                     src={profile && profile.avatar_url ? profile.avatar_url : '/default.png'}
                     className="rounded-full object-cover"
                     alt="avatar"
                     fill
                   />
-                </div> */}
-                <div>プロフィール</div>
+                </div>
               </Link>
             </div>
           ) : (
